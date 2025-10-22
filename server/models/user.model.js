141b5bdefa84b84ae01f5dce56368eac4b0c9e72
soumojit banerjee <soumojitbanerjee08@gmail.com>
2025-10-22 23:03:20 +0530

    added some routes

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema(
       type: Date,
       default: Date.now,
     },
+    gameHistory: {
+      type: Array,
+      default: [],
+    },
   },
   { timestamps: true }
 );
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -16,4 +16,21 @@ router.post("/signup", Controller.SignupController);
 // logout
 router.post("/logout", Controller.LogoutController);
 
+// create game
+router.post("/create-game")
+
+// join game
+router.post("/join-game")
+
+// get game
+router.get("/get-game")
+
+router.get("/vs-computer")
+
+router.get("/vs-player")
+
+router.get("/get-game-history")
+
+
+
 export default router;
