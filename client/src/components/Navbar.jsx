42c70edb0ece0e1271ce2147cd469ce4e7501c02
soumojit banerjee <soumojitbanerjee08@gmail.com>
2025-10-25 23:00:10 +0530

    Added Menu

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+
 
 const Navbar = () => {
   return (
     <nav>
-      <div className="logo"></div>
+      <div className="logo">Chess.com</div>
 
-      <div className="menu"></div>
+      <div className="menu">
+        <ul>
+          <li><a href="/">play</a></li>
+          <li><a href="/about">Watch</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </div>
 
       <div className="user"></div>
     </nav>
