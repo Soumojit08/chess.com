78e310517d362eb9fe36539cd68b6890ce5a533b
soumojit banerjee <soumojitbanerjee08@gmail.com>
2025-11-23 15:17:01 +0530

    fixed navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div className="h-screen w-full bg-background text-foreground">
+    <div className="h-screen w-full bg-obsidian text-foreground">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,42 +1,46 @@
-import { Link } from "react-router-dom";
-import { FaChessPawn } from "react-icons/fa";
-
-import { Button } from "@/components/ui/button";
+import CardNav from "./CardNav";
+import logo from "../assets/Logo.svg";
 
 const Navbar = () => {
+  const items = [
+    {
+      label: "About",
+      bgColor: "#0D0716",
+      textColor: "#fff",
+      links: [
+        { label: "Company", ariaLabel: "About Company" },
+        { label: "Careers", ariaLabel: "About Careers" },
+      ],
+    },
+    {
+      label: "Projects",
+      bgColor: "#170D27",
+      textColor: "#fff",
+      links: [
+        { label: "Featured", ariaLabel: "Featured Projects" },
+        { label: "Case Studies", ariaLabel: "Project Case Studies" },
+      ],
+    },
+    {
+      label: "Contact",
+      bgColor: "#271E37",
+      textColor: "#fff",
+      links: [
+        { label: "Email", ariaLabel: "Email us" },
+        { label: "Twitter", ariaLabel: "Twitter" },
+        { label: "LinkedIn", ariaLabel: "LinkedIn" },
+      ],
+    },
+  ];
+
   return (
-    <nav className="h-full w-1/7 bg-primary-foreground text-foreground flex flex-col items-start py-12 px-6 gap-10">
-      <div className="logo flex ">
-        <h1 className="tracking-widest font-extrabold text-3xl heading flex items-end justify-end ">
-          <span>
-            <FaChessPawn size={36} />
-          </span>
-          ChessMaster
-        </h1>
-      </div>
-
-      <div className="menu flex flex-col">
-        <ul>
-          <li>
-            <Link to="/play">Play</Link>
-          </li>
-          <li>
-            <Link to="/watch">Watch</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </div>
-
-      <div className="actions flex flex-col gap-2">
-        <Button variant="outline">Login</Button>
-        <Button>Signup</Button>
-        <Button variant="secondary" className="Toggle">
-          Dark Mode
-        </Button>
-      </div>
-    </nav>
+    <CardNav
+      logo={logo}
+      items={items}
+      baseColor="transparent"
+      menuColor="#fff"
+      ease="power3.out"
+    />
   );
 };
 
