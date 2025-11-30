a6b2ee3208d76a1e7f7465a371f411e6853f5163
soumojit banerjee <soumojitbanerjee08@gmail.com>
2025-11-30 11:22:30 +0530

    finish home page

--- client/src/components/CanvasElem.jsx
+++ client/src/components/CanvasElem.jsx
@@ -12,9 +12,14 @@ const CanvasElem = () => {
       dpr={[1, 2]}
       shadows
       camera={{ fov: 45 }}
-      style={{ position: "absolute" }}
+      style={{
+        position: "absolute",
+        height: "100%",
+        width: "100%",
+        zIndex: "10",
+      }}
     >
-      <Stage environment={null} shadows={"accumulative"} adjustCamera={0.6}>
+      <Stage environment={null} shadows={"accumulative"} adjustCamera={0.9}>
         {/* Rotation is [x, y, z] in radians. Math.PI / 4 is 45 degrees. */}
         <Model rotation={[0, -Math.PI / 4, 0]} />
       </Stage>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,33 @@
+import { Button } from "@/components/ui/button";
 import CanvasElem from "../components/CanvasElem";
 
 const Home = () => {
+  const handleClick = () => {
+    console.log("clicked");
+  };
   return (
-    <div className="min-h-screen bg-black text-white">
-      <CanvasElem />
+    <div className="min-h-screen bg-obsidian text-white position-relative ">
+      <div className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center z-20">
+        <h1 className="text-4xl font-bold font-sans uppercase">
+          Circle to Square
+        </h1>
+        <p className="font-mono text-muted-foreground text-center uppercase ">
+          simple.elegant.beautiful.
+        </p>
+
+        <Button
+          variant="outline"
+          className="mt-2 cursor-pointer"
+          onClick={handleClick}
+        >
+          Get Started
+        </Button>
+      </div>
+      <div className="block">
+        <CanvasElem />
+      </div>
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
