2c6ce6f38326ecfaec6d581f742f69d5f105ed37
soumojit banerjee <soumojitbanerjee08@gmail.com>
2025-11-30 10:30:41 +0530

    Fixed home screen model

--- client/src/components/CanvasElem.jsx
+++ client/src/components/CanvasElem.jsx
@@ -1,38 +1,30 @@
-import { Suspense } from "react";
-import { Canvas, useLoader, extend, useThree } from "@react-three/fiber";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import chessModel from "../assets/futuristic_chess_set_design.glb";
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
 
-// Register OrbitControls so it can be used as a JSX element
-extend({ OrbitControls });
-
-const CameraController = () => {
-  const { camera, gl } = useThree();
-  return <orbitControls args={[camera, gl.domElement]} enableDamping />;
-};
-
-const Model = () => {
-  const gltf = useLoader(GLTFLoader, chessModel);
-  return <primitive object={gltf.scene} />;
-};
+function Model(props) {
+  const { scene } = useGLTF("/chess.glb");
+  return <primitive object={scene} {...props} />;
+}
 
 const CanvasElem = () => {
   return (
-    <div className="h-screen w-full bg-[#1a1a1a]">
-      <Canvas camera={{ position: [0, 5, 10], fov: 45}}>
-        {/* Basic Lighting */}
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[10, 10, 5]} intensity={1} />
-        <pointLight position={[-10, -10, -10]} intensity={0.5} />
-
-        <Suspense fallback={null}>
+    <Canvas
+      dpr={[1, 2]}
+      shadows
+      camera={{ fov: 45 }}
+      style={{ position: "absolute" }}
+    >
+      <PresentationControls
+        rotation={[0,0,0]}
+        damping={0.2}
+        // speed={1.5}
+        // global
+      >
+        <Stage environment={null}>
           <Model />
-        </Suspense>
-
-        <CameraController />
-      </Canvas>
-    </div>
+        </Stage>
+      </PresentationControls>
+    </Canvas>
   );
 };
 
