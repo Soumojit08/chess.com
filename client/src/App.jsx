015362199274415077c2362e911dc42aba78a44c
soumojit banerjee <soumojitbanerjee08@gmail.com>
2025-12-03 01:12:17 +0530

    Added Loaded

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Preloader from "./components/Preloader";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
   return (
-    <div className="h-screen w-full bg-obsidian text-foreground">
+    <div className="h-screen w-full bg-obsidian text-foreground relative">
+      {loading && <Preloader onComplete={() => setLoading(false)} />}
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,10 +8,10 @@ const Home = () => {
   return (
     <div className="h-screen bg-obsidian text-white position-relative ">
       <div className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center z-20">
-        <h1 className="text-4xl font-bold font-sans uppercase">
+        <h1 className="text-xl font-bold font-sans uppercase md:text-4xl text-center">
           Circle to Square
         </h1>
-        <p className="font-mono text-muted-foreground text-center uppercase ">
+        <p className="font-mono text-muted-foreground text-center uppercase md:text-xl text-xs">
           simple.elegant.beautiful.
         </p>
 
