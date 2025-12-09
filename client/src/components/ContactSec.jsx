ceddeb1cde26e2db421175c6a86cac023b8fab4f
soumojit banerjee <soumojitbanerjee08@gmail.com>
2025-12-09 20:48:28 +0530

    fix spacing in gallery sec

--- client/src/components/ContactSec.jsx
+++ client/src/components/ContactSec.jsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 const ContactSec = () => {
   return (
-    <section className="w-full py-24 px-8 md:px-24 bg-white text-black border-t border-gray-100">
+    <section className="w-full pt-0 pb-24 px-8 md:px-24 bg-white text-black border-t border-gray-100">
       <div className="flex flex-col md:flex-row items-end justify-between">
         <div className="w-full md:w-1/2">
           <h2 className="text-2xl font-mono tracking-wider mb-4 text-black">
--- client/src/components/GallerySec.jsx
+++ client/src/components/GallerySec.jsx
@@ -2,12 +2,12 @@ import MagicBento from "./MagicBento";
 
 const GallerySec = () => {
   return (
-    <section className="w-full py-24 px-8 md:px-24 bg-white text-black">
-      <h2 className="text-2xl font-mono tracking-widest mb-4 text-black">
+    <section className="w-full py-12 px-8 md:px-24 bg-white text-black">
+      <h2 className="text-2xl font-mono tracking-widest mb-8 text-black">
         THE GALLERY
       </h2>
 
-      <div className="w-full h-auto flex justify-center">
+      <div className="w-full h-auto flex justify-center items-center">
         <MagicBento
           textAutoHide={false}
           enableStars={true}
