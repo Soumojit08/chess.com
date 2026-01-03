3019633a542ae83c8bb90ea1060081cc8812fbfd
soumojit banerjee <soumojitbanerjee08@gmail.com>
2026-01-03 21:41:01 +0530

    Remove preloader

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,6 @@
-import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import Preloader from "./components/Preloader";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
@@ -11,11 +9,8 @@ import { Toaster } from "react-hot-toast";
 import AuthSync from "./providers/AuthSync";
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
-
   return (
     <div className="h-screen w-full bg-obsidian text-foreground relative">
-      {loading && <Preloader onComplete={() => setLoading(false)} />}
       <AuthSync />
       <Navbar />
       <Routes>
