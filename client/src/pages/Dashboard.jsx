7a0a95694db5bf6fbaa8621439c53b8669bb46d8
soumojit banerjee <soumojitbanerjee08@gmail.com>
2026-02-15 01:47:56 +0530

    addded layout in dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -6,7 +6,7 @@ import {
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { useSelector } from "react-redux";
-import Avtar from "@/components/Avtar";
+import img from "../assets/home page.webp";
 
 const Dashboard = () => {
   const user = useSelector((state) => state.auth.user);
@@ -14,8 +14,10 @@ const Dashboard = () => {
   return (
     <div className="h-screen w-full bg-background text-foreground flex flex-wrap font-sans pt-15">
       <div className="w-full flex h-[65%] p-2 pb-1 gap-2">
-        <section className="border-red-700 border-2 w-[60%]"></section>
-        <section className="border-red-700 border-2 w-[40%]"></section>
+        <section className="border-red-700 border-2 w-[50%]">
+          <img src={img} className="w-full h-full object-fit"/>
+        </section>
+        <section className="border-red-700 border-2 w-[50%]"></section>
       </div>
       <div className="w-full flex h-[35%] p-2 pt-1 gap-2">
         <section className="border-red-700 border-2 w-[25%]"></section>
